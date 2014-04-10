c69256afca4e3de3bcaf1afc5038196ab522aa4a
Joel Carranza <joel.carranza@gmail.com>
2014-04-10 18:34:34 -0500

    modify pushgist for pinswift

--- pushgist.sh
+++ pushgist.sh
@@ -5,4 +5,4 @@ OUT=build
 ./build.sh
 jist $OUT/bookmark.js -u cfd44a7a72442bb734eb
 jist $OUT/readlater.js -u 98d36e72c48d1795553a
-jist $OUT/pinbook.js -u ff3d0c2e84def744a53d
+jist $OUT/pinswift.js -u ff3d0c2e84def744a53d
